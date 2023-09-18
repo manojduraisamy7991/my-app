11b7bba99d5934a917d3295d78c293b864c50b98
manojduraisamy7991 <manojduraisamy7991@gmail.com>
2023-09-18 21:25:51 +0530

    add

--- src/containers/ImdbHome.jsx
+++ src/containers/ImdbHome.jsx
@@ -2,22 +2,11 @@
 import * as React from 'react';
 import PhotoGallery from '../components/PhotoGallery';
 
-export default function ResponsiveGrid() {
+export default function ImdbHome() {
   return (
           <>
          <PhotoGallery />
           </>
   );
 }
-    {/* <Stack spacing={2} direction="row" style={{ borderRadius:11,  textAlign: 'inherit',}}>
-         <CardMedia
-        component="img"
-        sx={{ width: 181, height:150 , borderRadius:8}}
-        image={img}
-        alt="Live from space album cover"
-      />
-          <Box sx={{ maxWidth: 400 }}>
-          <Typography variant='h5' sx={{ overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}>{message}</Typography>
-          <Typography variant='h6' sx={{ overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}>{message}</Typography>
-        </Box>
-        </Stack> */}
+ 
